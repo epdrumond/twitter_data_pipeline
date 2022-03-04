14d79c3d6bc069c9bdfefaf644d9eb3cf559ca0c
Edilson <epdrumond@gmail.com>
2022-03-04 19:46:09 -0300

    Changing paths to a more generic format

--- airflow/dags/twitter_dag.py
+++ airflow/dags/twitter_dag.py
@@ -1,32 +1,65 @@
 from airflow.models import DAG
 from airflow.operators.alura import TwitterOperator
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.utils.dates import days_ago
 
 from datetime import datetime
 from os.path import join
+from pathlib import Path
+
+ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(6)
+}
+BASE_FOLDER = join(
+    str(Path('~/Documents').expanduser()),
+    'data_pipeline/datalake/{stage}/fortaleza_ec/{partition}'
+)
+PARTITION_FOLDER = 'extract_date={{ ds }}'
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.00Z'
 
 datalake_path = '/home/edilson/Documents/data_pipeline/datalake'
-with DAG(dag_id = 'twitter_dag', start_date = datetime.now()) as dag:
+with DAG(
+    dag_id = 'twitter_dag',
+    default_args = ARGS,
+    schedule_interval = '0 9 * * *',
+    max_active_runs = 1
+) as dag:
     twitter_operator = TwitterOperator(
-    task_id = 'retrieve_twitter_data',
-    query = '@FortalezaEC',
-    file_path = join(
-        datalake_path,
-        'fortaleza_ec',
-        'extract_date={{ ds }}',
-        'twitter_dump_{{ ds_nodash }}.json'
-    ))
+        task_id = 'retrieve_twitter_data',
+        query = '@FortalezaEC',
+        file_path = join(
+            BASE_FOLDER.format(stage = 'bronze', partition = PARTITION_FOLDER),
+            'twitter_dump_{{ ds_nodash }}.json'
+        ),
+        start_time = (
+            "{{"
+            f"execution_date.strftime('{ TIMESTAMP_FORMAT }')"
+            "}}"
+        ),
+        end_time = (
+            "{{"
+            f"next_execution_date.strftime('{ TIMESTAMP_FORMAT }')"
+            "}}"
+        )
+    )
 
     twitter_transform = SparkSubmitOperator(
         task_id = 'transform_tweet_data',
-        application = '/home/edilson/Documents/data_pipeline/spark/transformation.py',
+        application = join(
+            str(Path(__file__).parents[2]),
+            'spark/transformation.py'
+        ),
         name = 'twitter_transformation',
         application_args = [
             '--src',
-            '/home/edilson/Documents/data_pipeline/datalake/bronze/fortaleza_ec/extract_date=2022-02-26',
+            BASE_FOLDER.format(stage = 'bronze', partition = 'PARTITION_FOLDER'),
             '--dest',
-            '/home/edilson/Documents/data_pipeline/datalake/silver/fortaleza_ec',
+            BASE_FOLDER.format(stage = 'silver', partition = ''),
             '--process-date',
             '{{ ds }}'
         ]
     )
+
+    twitter_operator >> twitter_transform
